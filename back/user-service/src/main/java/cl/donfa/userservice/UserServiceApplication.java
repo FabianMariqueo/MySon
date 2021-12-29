7de0a7cefa5ea015900db00941a71824901cfc77
Fabian <f.mariqueo01@ufromail.cl>
2021-12-29 19:08:50 -0300

    Feature: intialize client application

---
+++ back/user-service/src/main/java/cl/donfa/userservice/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package cl.donfa.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
---
+++ back/user-service/src/test/java/cl/donfa/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.donfa.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
